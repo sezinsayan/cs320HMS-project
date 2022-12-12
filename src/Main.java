5a73eedaa898795cf7975bed3e276f6cf67bdff3
sezinsayan <sezin.sayan@ozu.edu.tr>
2022-12-12 19:24:34 +0300

    main update

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,9 @@ public class Main extends JFrame {
     Main(){
         setSize(1366,565);
         setVisible(true);
+
     }
-    public static void main(String[] args) {
+    public static void main(String[] args) { new Main();
 
     }
 }
